622351c1573680f4adebee974354a34872a635ba
Zeziulin Sergey <kotleta353@gmail.com>
2023-11-02 00:54:35 +0600

    final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         FormDate birthday = new FormDate();
-        birthday.day = "13";
-        birthday.month = "6";
-        birthday.year = "1999";
+        birthday.day = 13;
+        birthday.month = 6;
+        birthday.year = 1999;
 
         Post post = new Post();
         post.name = "Иван";
@@ -12,6 +12,5 @@ public class Main {
         post.phone = "+7 (999)-999-99-99";
         post.surname = "Иванов";
         post.subscription = true;
-
     }
 }
